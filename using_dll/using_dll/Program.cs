ba8da56e7c65089263ad223ce9a335738eeb8dda
timmyboy113 <flarp113@googlemail.com>
2016-03-22 21:07:31 +0100

    init
    init

---
+++ using_dll/using_dll/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib;
+
+namespace using_dll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine( lib.Test.echo("Ich bin ein Text der gleich ausgegeben wird :)"));
+            Console.WriteLine(lib.Test.add(13, 37));
+            Console.ReadLine();
+        }
+    }
+}
